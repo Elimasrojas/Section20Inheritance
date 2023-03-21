993ff2b6d2016e70b53b57eb33f8925f06e3125b
Elimasrojas <elimasrojas@gmail.com>
2023-03-20 19:01:46 -0500

    first commit

---
+++ src/test/java/com/elr/elr/ElrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elr.elr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
